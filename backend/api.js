74cf3c8c8c045641a889de9f375dc8429cef5b5b
fulopco <you@example.com>
2023-03-27 22:07:49 +0200

    add button and input HTML element

--- backend/api.js
+++ backend/api.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const allergens = require("./allergens.json");
 const beers = require("./beers.json");
-
 const model = require("./model.js");
 
 // API backend - path kezelő
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,27 @@
+const objectToSend =
+{
+  id: "",
+  beers: [
+    //{id: 1, amount: 2}
+  ],
+  date: {
+    year: 2022,
+    month: 6,
+    day: 7,
+    hour: 18,
+    minute: 47
+  },
+  customer: {
+    name: "",
+    email: "",
+    address: {
+      city: "",
+      street: ""
+    }
+  }
+}
+
+
 // 1) Sörök meghívása
 async function fetchBeers() {
   const response = await fetch("/api/beers");
@@ -28,20 +52,43 @@ async function displayBeers() {
   const rootE = document.getElementById("root");
 
   const beersInHTMLStructure = await Promise.all(
-    data.beers.map(
-      async (beer) =>
-        `<div id=beer${beer.id}>
-    <h1>${beer.name}</h1>
-    <h2>${beer.brewery}</h2>
-    <p class ="detailsType">Style: ${beer.type}</p>
-    <p class ="detailsAbv">ABV: ${beer.abv}%</p>
-    <p class ="detailsPrice">Price: ${beer.price}€</p>
-    <p class ="detailsAllergens">Allergens: ${await displayAllergensNames(beer)}</p>
-    </div>`
+    data.beers.map( async (beer) =>
+        `<div id="beer${beer.id}">
+        <h1>${beer.name}</h1>
+        <h2>${beer.brewery}</h2>
+        <p class="detailsType">Style: ${beer.type}</p>
+        <p class="detailsAbv">ABV: ${beer.abv}%</p>
+        <p class="detailsPrice">Price: ${beer.price}€</p>
+        <p class="detailsAllergens">Allergens: ${await displayAllergensNames(beer)}</p>
+        ${await buttonHandler(beer.id)}
+        ${await amountInputHandler(beer.id)}
+        </div>`
     )
   );
   rootE.insertAdjacentHTML("beforeend", beersInHTMLStructure.join(""));
 }
+
+async function buttonHandler(IDs) {
+  document.getElementById(`beer${IDs}`)
+  .insertAdjacentHTML("beforeend", `<button id="beerButtonID${IDs}">Click to order!</button>`)
+}
+
+async function amountInputHandler(IDs) {
+  document.getElementById(`beer${IDs}`)
+  .insertAdjacentHTML("beforeend",`<input id="beerInputID${IDs}" type="number" placeholder="Amount..."></input>`)
+
+}
+
+async function fuck (IDs) {
+  document.getElementById(`beerInputID${IDs}`).addEventListener("blur", () => {
+    let valueOfInput = document.getElementById(`beerInputID${IDs}`).value
+    objectToSend.beers.push({ id: IDs, amount: valueOfInput })
+    console.log(objectToSend);
+  })
+}
+
+
+
 
 function main() {
   displayBeers();
